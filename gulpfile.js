2777be5fe0d9072658c74b08036f890830cb5ce8
Dmitry <starmeebst@mail.ru>
2021-04-10 22:49:56 +0300

    10.04.2021 22:49

--- gulpfile.js
+++ gulpfile.js
@@ -12,27 +12,23 @@ gulp.task('server', function() {
             baseDir: "src"
         }
     });
+
+    gulp.watch('src/*.html').on('change', browserSync.reload); //изменения в html 
 });
 
-gulp.task('styles' , function(){
-    return gulp.src("src/sass/*.+(scss|sass)")
-            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))  // компиляция файла и сжатие
-            .pipe(rename({
-                prefix: "",
-                suffix: ".min", // оконцовка min
-              }))
-            .pipe(autoprefixer({
-                cascade: false
-            })) //префиксы
-            .pipe(cleanCSS({compatibility: 'ie8'})) //clean css очищение
-            .pipe(gulp.dest("src/css")) // сохранение файла
-            .pipe(browserSync.stream()) // перезагрузка страницы
-
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function(){
-    gulp.watch('src/sass/*.+(scss|sass)' , gulp.parallel('styles')) //следит за изменениями в файле sass
-    gulp.watch('src/*.html').on('change', browserSync.reload); //изменения в html 
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles')); //следит за изменениями в файле sass но и папки внутри
+     
 });
 
 
